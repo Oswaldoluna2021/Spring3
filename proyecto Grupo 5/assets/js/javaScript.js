a83b5ecd8a46094442407c9d52ac43468621e004
Oswaldoluna2021 <oswaldole2019@gmail.com>
2021-10-22 02:50:27 -0500

    main

---
+++ proyecto Grupo 5/assets/js/javaScript.js
@@ -0,0 +1,9 @@
+//<script type="text/javascript">
+  function mostrarFormulario() {
+    document.getElementById('ocultar').style.display = 'block';
+}
+
+function ocultarFormulario() {
+    document.getElementById('ocultar').style.display = 'block';
+}
+//</script>
